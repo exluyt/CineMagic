68e201b28ff8fc5e55cf8e7c1066cc9abee57c97
Ignacio Bermejo <nachusky34@gmail.com>
2024-05-29 00:21:28 +0200

    Se ha introducido frames y responsive a pelicula
    Cambiar los nombres de las variables

--- JS/peliculas.js
+++ JS/peliculas.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
+    var pelicula = sessionStorage.getItem('pelicula');
+    var titulo = document.getElementById('titulo');
+    var descripcion = document.getElementById('descripcionP');
+    var fecha = document.getElementById('fechaP');
+    var director = document.getElementById('director');
+    var video = document.getElementById('video');
     var estrellas1 = [
         document.getElementById('estrella1'),
         document.getElementById('estrella2'),
@@ -6,21 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById('estrella4'),
         document.getElementById('estrella5')
     ];
-
-    var count = document.getElementById('estrellas');
-    var rating = parseInt(count.getAttribute('data-rating'));
-
-    for (let i = 0; i < rating; i++) {
-        estrellas1[i].setAttribute('src', 'img/estrella.png');
-        estrellas1[i].setAttribute('class', 'estrellaGold');
-    }
-
-    var pelicula = sessionStorage.getItem('pelicula');
-    var titulo = document.getElementById('titulo');
-    var descripcion = document.getElementById('descripcionP');
-    var fecha = document.getElementById('fechaP');
-    var director = document.getElementById('director');
-    var video = document.getElementById('video');
+    
 
     switch (pelicula) {
         case 1:
@@ -30,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
             director.innerText = ''
             video.setAttribute('src', '')
             video.setAttribute('title', '')
+            count.setAttribute('data-rating', '');
             break;
         case 2:
             titulo.innerText = ''
@@ -38,6 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
             director.innerText = ''
             video.setAttribute('src', '')
             video.setAttribute('title', '')
+            count.setAttribute('data-rating', '');
             break;
         case 3:
             titulo.innerText = ''
@@ -46,6 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
             director.innerText = ''
             video.setAttribute('src', '')
             video.setAttribute('title', '')
+            count.setAttribute('data-rating', '');
             break;
         case 4:
             titulo.innerText = ''
@@ -54,6 +49,7 @@ document.addEventListener("DOMContentLoaded", function () {
             director.innerText = ''
             video.setAttribute('src', '')
             video.setAttribute('title', '')
+            count.setAttribute('data-rating', '');
             break;
         case 5:
             titulo.innerText = ''
@@ -62,6 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
             director.innerText = ''
             video.setAttribute('src', '')
             video.setAttribute('title', '')
+            count.setAttribute('data-rating', '');
             break;
         case 6:
             titulo.innerText = ''
@@ -70,6 +67,7 @@ document.addEventListener("DOMContentLoaded", function () {
             director.innerText = ''
             video.setAttribute('src', '')
             video.setAttribute('title', '')
+            count.setAttribute('data-rating', '');
             break;
         case 7:
             titulo.innerText = ''
@@ -78,6 +76,7 @@ document.addEventListener("DOMContentLoaded", function () {
             director.innerText = ''
             video.setAttribute('src', '')
             video.setAttribute('title', '')
+            count.setAttribute('data-rating', '');
             break;
         case 8:
             titulo.innerText = ''
@@ -86,6 +85,14 @@ document.addEventListener("DOMContentLoaded", function () {
             director.innerText = ''
             video.setAttribute('src', '')
             video.setAttribute('title', '')
+            count.setAttribute('data-rating', '');
             break;
+    }
+    
+    var count = document.getElementById('estrellas');
+    var rating = parseInt(count.getAttribute('data-rating'));
+    for (let i = 0; i < rating; i++) {
+        estrellas1[i].setAttribute('src', 'img/estrella.png');
+        estrellas1[i].setAttribute('class', 'estrellaGold');
     }
 });
